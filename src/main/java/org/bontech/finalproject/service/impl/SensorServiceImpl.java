27ea0c7dad088f59931e479dfe377c21cafbf536
mostafa <4mocpmo2@gmail.com>
2021-09-22 16:50:57 +0330

    SensorServiceImpl.java was refactored

--- src/main/java/org/bontech/finalproject/service/impl/SensorServiceImpl.java
+++ src/main/java/org/bontech/finalproject/service/impl/SensorServiceImpl.java
@@ -15,7 +15,6 @@ import org.bontech.finalproject.service.base.SensorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.Optional;
 import java.util.Random;
 

6303c4b1461c2ceaffed5b06ce2b92736731dfe0
mostafa <4mocpmo2@gmail.com>
2021-09-22 16:46:14 +0330

    siloService and StorageService were constructed

--- src/main/java/org/bontech/finalproject/service/base/SiloService.java
+++ src/main/java/org/bontech/finalproject/service/base/SiloService.java
@@ -1,4 +1,17 @@
 package org.bontech.finalproject.service.base;
 
+import java.util.List;
+
+import org.bontech.finalproject.model.*;
+import org.bontech.finalproject.model.dto.*;
+
 public interface SiloService {
+
+	Silo createSilo(CreateSilo createSilo);
+	void updateSilo(UpdateSilo updateSilo);
+	void deleteSilo(DeleteSilo deleteSilo);
+	Silo findSilo(Long siloId);
+	List<ChangesOfSilo> getHistory(Long siloId,Integer n);
+	Silo addOrHarvest(Long siloId);
+
 }
--- src/main/java/org/bontech/finalproject/service/base/StorageService.java
+++ src/main/java/org/bontech/finalproject/service/base/StorageService.java
@@ -1,4 +1,16 @@
 package org.bontech.finalproject.service.base;
 
+import org.bontech.finalproject.model.*;
+import org.bontech.finalproject.model.dto.*;
+import java.util.List;
+
 public interface StorageService {
+
+	Storage creteStorage(CreateStorage creteStorage);
+	void deleteStorage(DeleteStorage deleteStorage);
+	Storage updateStorage(UpdateStorage updateStorage);
+	Storage	findStorage(Long storageId);
+	Long getCurrentStored(Long storageId);
+	List<Silo> getAllSilo(Long storageId);
+
 }

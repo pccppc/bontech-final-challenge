2931746ee6ede67742e24ae070f5ad9e3efe689f
mostafa <4mocpmo2@gmail.com>
2021-09-22 17:24:38 +0330

    documentation of SiloService was added
    documentation of StorageService was added

--- src/main/java/org/bontech/finalproject/service/base/SiloService.java
+++ src/main/java/org/bontech/finalproject/service/base/SiloService.java
@@ -7,11 +7,51 @@ import org.bontech.finalproject.model.dto.*;
 
 public interface SiloService {
 
+	/**
+	 * create a silo and insert that into database
+	 * @param createSilo include name and maximumWeigh of silo
+	 * @return Silo 
+	 */
 	Silo createSilo(CreateSilo createSilo);
+
+	/**
+	 * update a silo and save changes into database
+	 * @param updateSilo information of silo and siloId for search and update
+	 */
+
 	void updateSilo(UpdateSilo updateSilo);
+
+	/**
+	 * delete a silo from database
+	 * @param deleteSilo for search silo and delete it
+	 */
+
 	void deleteSilo(DeleteSilo deleteSilo);
+
+	/**
+	 * find a silo
+	 * @param siloId for search into database and select it with siloId
+	 * @return Silo
+	 */
+
 	Silo findSilo(Long siloId);
+
+	/**
+	 * fnid (add or harvest) history of silo from table Changes_of_silo and return n record from that
+	 * @param siloId for check silo existence
+	 * @param n for return n record of result (handle it into database query)
+	 * @return List<ChangesOfSilo> list of history record
+	 */
+
 	List<ChangesOfSilo> getHistory(Long siloId,Integer n);
+
+	/**
+	 * add or harvest from silo, sensor of silo sense two time and check maximum and minimum and save history 
+	 * @param siloId for search silo into database
+	 * @return Silo
+	 */
+
 	Silo addOrHarvest(Long siloId);
+
 
 }
--- src/main/java/org/bontech/finalproject/service/base/StorageService.java
+++ src/main/java/org/bontech/finalproject/service/base/StorageService.java
@@ -6,11 +6,49 @@ import java.util.List;
 
 public interface StorageService {
 
-	Storage creteStorage(CreateStorage creteStorage);
+	/**
+	 * create a storage and insert into database 
+	 * @param createStorage include information of storage 
+	 * @return Storage created Storage
+	 */
+
+	Storage creteStorage(CreateStorage createStorage);
+
+	/**
+	 * find a storage from database and delete that
+	 * @param deleteStorage include storageId for search storage 
+	 */
+
 	void deleteStorage(DeleteStorage deleteStorage);
+
+	/**
+	 * find a storage and update that
+	 * @param updateStorage include id for search and name for update
+	 * @return Storage 
+	 */
+
 	Storage updateStorage(UpdateStorage updateStorage);
+
+	/**
+	 * find a storage
+	 * @param storageId for search into database
+	 * @return Storage
+	 */
+
 	Storage	findStorage(Long storageId);
+
+	/**
+	 * find all silo of Selectd storage with storageId and return sum of currentWeight of all silo
+	 * @param storageId for search storage from database
+	 * @return Long result of sum opperation on all silo from selected storage
+	 */
 	Long getCurrentStored(Long storageId);
+
+	/**
+	 * find all silo from selected storage
+	 * @param storageId for search storage
+	 * @return List<Silo>
+	 */
 	List<Silo> getAllSilo(Long storageId);
 
 }

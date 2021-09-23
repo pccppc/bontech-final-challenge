0db7e866c76a7a0317c3466a5e73ce3ae9610e9f
mostafa <4mocpmo2@gmail.com>
2021-09-23 11:45:13 +0330

    user model was creted

---
+++ src/main/java/org/bontech/finalproject/model/User.java
@@ -0,0 +1,26 @@
+package org.bontech.finalproject.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String username;
+    private String password;
+    private boolean enabled;
+    private String role;
+
+}

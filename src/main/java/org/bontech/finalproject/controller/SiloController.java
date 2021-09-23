0cb29cc9f1ae860350390b712998193e23898ad0
mostafa <4mocpmo2@gmail.com>
2021-09-23 11:45:46 +0330

    controllers were created

---
+++ src/main/java/org/bontech/finalproject/controller/SiloController.java
@@ -0,0 +1,7 @@
+package org.bontech.finalproject.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SiloController {
+}
---
+++ src/main/java/org/bontech/finalproject/controller/StorageController.java
@@ -0,0 +1,8 @@
+package org.bontech.finalproject.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StorageController {
+
+}
